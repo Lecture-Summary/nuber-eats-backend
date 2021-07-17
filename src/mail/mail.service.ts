b7d0bdc9449a0bbd927937320ec1eac70d2105ff
이호진 <kiss0104040@gmail.com>
2021-07-17 23:46:12 +0900

    Beautiful Emails

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -8,16 +8,16 @@ import { MailModuleOptions } from './mail.interfaces';
 export class MailService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
-  ) {
-    this.sendEmail('testing', 'test');
-  }
+  ) {}
 
-  private async sendEmail(subject: string, content: string) {
+  private async sendEmail(subject: string, template: string) {
     const form = new FormData();
     form.append('from', `Nuber-Eats Clone <mailgun@${this.options.domain}>`);
-    form.append('to', `kiss0104040@naver.com`);
+    form.append('to', `kiss0104040@gmail.com`);
     form.append('subject', subject);
-    form.append('text', content);
+    form.append('template', template);
+    form.append('v:code', 'asasas');
+    form.append('v:username', 'HoJin');
 
     const response = await got(
       `https://api.mailgun.net/v3/${this.options.domain}/messages`,
