86c4ed1f1d5c4f09e62b32655841d204d88cd78b
이호진 <kiss0104040@gmail.com>
2021-07-29 20:35:47 +0900

    11.13 getOrders and getOrder

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -5,6 +5,7 @@ import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User, UserRole } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders-dto';
 import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
@@ -88,11 +89,11 @@ export class OrderService {
       let orders: Order[];
       if (user.role === UserRole.Client) {
         orders = await this.orders.find({
-          where: { customer: user },
+          where: { customer: user, ...(status && { status }) },
         });
       } else if (user.role === UserRole.Delivery) {
         orders = await this.orders.find({
-          where: { driver: user },
+          where: { driver: user, ...(status && { status }) },
         });
       } else if (user.role === UserRole.Owner) {
         const restaurants = await this.restaurants.find({
@@ -100,10 +101,46 @@ export class OrderService {
           relations: ['orders'],
         });
         orders = restaurants.map(restaurant => restaurant.orders).flat(1);
+        if (status) {
+          orders = orders.filter(order => order.status === status);
+        }
       }
       return { ok: true, orders };
     } catch {
       return { ok: false, error: 'Could not get orders' };
+    }
+  }
+
+  async getOrder(
+    user: User,
+    { id: orderId }: GetOrderInput,
+  ): Promise<GetOrderOutput> {
+    try {
+      const order = await this.orders.findOne(orderId, {
+        relations: ['restaurant'],
+      });
+      if (!order) {
+        return { ok: false, error: 'Order not found.' };
+      }
+      let canSee = true;
+      if (user.role === UserRole.Client && order.customerId !== user.id) {
+        canSee = false;
+      }
+      if (user.role === UserRole.Delivery && order.driverId !== user.id) {
+        canSee = false;
+      }
+      if (
+        user.role === UserRole.Owner &&
+        order.restaurant.ownerId !== user.id
+      ) {
+        canSee = false;
+      }
+      if (!canSee) {
+        return { ok: false, error: 'You cant see that.' };
+      }
+      return { ok: true, order };
+    } catch {
+      return { ok: false, error: 'Could not load order.' };
     }
   }
 }
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -38,12 +38,12 @@ export class Restaurant extends CoreEntity {
   @ManyToOne(() => User, user => user.restaurants, { onDelete: 'CASCADE' })
   owner: User;
 
+  @RelationId((restaurant: Restaurant) => restaurant.owner)
+  ownerId: number;
+
   @Field(type => [Order])
   @OneToMany(type => Order, order => order.restaurant)
   orders: Order[];
-
-  @RelationId((restaurant: Restaurant) => restaurant.owner)
-  ownerId: number;
 
   @Field(type => [Dish])
   @OneToMany(type => Dish, dish => dish.restaurant)
